41b0226d87131eae2f13b160863a7bba9b589f40
SmallNic <nas231@nyu.edu>
2015-09-24 19:44:41 -0400

    playing with descriptions. asynchronicity issues

--- app.js
+++ app.js
@@ -6,49 +6,75 @@ app.controller('MainCtrl', [
   '$scope',
   'pic_ids',
   'pic_urls',
-  function($scope, pic_ids, pic_urls){
+  'pic_descrip',
+  function($scope, pic_ids, pic_urls, pic_descrip){
     $scope.photo_ids = [];
     $scope.photo_urls = [];
-    $scope.photo_url = ""
+    $scope.photo_descrips = [];
+    $scope.photos = [];
+    $scope.photo = ""
     $scope.newPhoto = function(){
-      console.log("click")
-      $scope.photo_url = $scope.photo_urls[Math.floor(Math.random()*$scope.photo_urls.length)];
+      // console.log("click")
+      $scope.photo = $scope.photos[Math.floor(Math.random()*$scope.photos.length)];
     }
 
     pic_ids.get().then(function(response){
-      data = JSON.parse(response.data.substring(14, response.data.length-1))
-      console.log("data:", data)
-
-      for(var i = 0; i < data.photoset.photo.length; i ++){
-
-        //select photo id and store all of them in an array
-        photo_id = data.photoset.photo[i].id
+      photosetData = JSON.parse(response.data.substring(14, response.data.length-1))
+      for(var i = 0; i < photosetData.photoset.photo.length; i ++){
+        photo_id = photosetData.photoset.photo[i].id
         $scope.photo_ids.push(photo_id)
+        console.log(i, "photo_id", photo_id)
+      }
+    }).then(function(){
+      numPhotos = $scope.photo_ids.length
+      for(var i = 0; i < numPhotos; i ++){
+        photo_id = $scope.photo_ids[i]
 
         pic_urls.get(photo_id).then(function(response){
           picSizeData = JSON.parse(response.data.substring(14, response.data.length-1))
-          for (var j =0; j < picSizeData.sizes.size.length; j++){
-            // console.log(picSizeData.sizes.size[j].label)
-            if (picSizeData.sizes.size[j].label === "Original"){
-              console.log(picSizeData.sizes.size[j].source)
-              $scope.photo_urls.push(picSizeData.sizes.size[j].source)
+          url = picSizeData.sizes.size[10].source;
+          console.log("url:", url)
+          $scope.photo_urls.push(url)
+        }).then(function(){
+
+          pic_descrip.get(photo_id).then(function(response){
+            picData = JSON.parse(response.data.substring(14, response.data.length-1))
+            descrip = picData.photo.description._content;
+            console.log(photo_id, "descrip:", descrip)
+            $scope.photo_descrips.push(descrip)
+          }).then(function(){
+
+            for(var i = 0; i < $scope.photo_ids.length; i ++){
+              $scope.photos.push({id:$scope.photo_ids[i], url:$scope.photo_urls[i], descrip:$scope.photo_descrips[i]})
             }
-          }
-          console.log("$scope.photo_urls", $scope.photo_urls)
-
-        }).then(function(response){
-          $scope.photo_url  = $scope.photo_urls[Math.floor(Math.random()*$scope.photo_urls.length)];
-          console.log("$scope.photo_url", $scope.photo_url)
-
+            $scope.photo  = $scope.photos[Math.floor(Math.random()*$scope.photos.length)];
+            console.log("$scope.photos", $scope.photos)
+            console.log("$scope.photo", $scope.photo)
+          })
         })
       }
-
-      console.log("$scope.photo_ids", $scope.photo_ids)
-
     })
-
   }
 ])
+
+          /*
+          console.log("picSizeData", picSizeData)
+          for (var j =0; j < picSizeData.sizes.size.length; j++){
+            // console.log(picSizeData.sizes.size[j].label)
+            if (picSizeData.sizes.size[10].label
+              console.log(i,"",url)
+
+              pic_descrip.get(photo_id).then(function(response){
+                picData = JSON.parse(response.data.substring(14, response.data.length-1))
+                console.log("picData", picData)
+                descrip = picData.photo.description._content;
+                console.log("url", url)
+                console.log("descrip", descrip)
+                $scope.photo_urls.push({url:url, descrip:descrip})
+              })
+            }
+
+          */
 
 
 //Setup Factory to hold the data
@@ -73,7 +99,6 @@ app.factory('pic_ids', ['$http', function($http){
 
 
 app.factory('pic_urls', ['$http', function($http){
-  console.log("this", $(this))
   return {
     get: function(pic_url) {
       return $http({
@@ -84,6 +109,17 @@ app.factory('pic_urls', ['$http', function($http){
   }
 }]);
 
+
+app.factory('pic_descrip', ['$http', function($http){
+  return {
+    get: function(pic_id) {
+      return $http({
+        url: 'https://api.flickr.com/services/rest/?method=flickr.photos.getInfo&api_key=f171555321fcc7e567ecafaaf8c27c38&photo_id='+pic_id+'&format=json',
+        method: 'get',
+      })
+    }
+  }
+}]);
 
 //Setup Routes
 app.config([
